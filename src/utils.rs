4d7887041f8044b4a8aeaed8444eb98cbb0eb455
matszpk <matszpk@interia.pl>
2024-08-23 19:32:17 +0200

    Add some new stuff to utils.

--- src/utils.rs
+++ src/utils.rs
@@ -25,12 +25,6 @@ use infmachine_gen::*;
 // END_DP_FORM: [NP0_0, NP0_1, .., NP0_T, END0, NP1_0, NP1_1, .., NP1_T, END1, .....].
 // T - (data_part_len + cell_len - 1) / cell_len.  Number of required cells to store data part.
 
-// InitMemAddressEndPosState - initialize memory address end position from memory.
-// Information about MemAddressEndPos in memory:
-// At memory address 0: sequences of values between 1..=MAX and one zero,
-// MemAddressPosEndPos is sum of non-zero cells.
-// If cell_len=1 then: sequences of 1 and one zero. MemAddressPosEndPos is number of 1's.
-
 const fn calc_log_bits(n: usize) -> usize {
     let nbits = usize::BITS - n.leading_zeros();
     if (1 << (nbits - 1)) == n {
@@ -40,32 +34,41 @@ const fn calc_log_bits(n: usize) -> usize {
     }
 }
 
-#[derive(Clone)]
-pub struct InitMemAddressEndPosStage {
-    stage: UDynVarSys,   // stage indicator
-    int_stage: U3VarSys, // internal stage indicator - only for this stage.
-    state: U2VarSys,     // state for stage
-}
-
-impl InitMemAddressEndPosStage {
-    pub fn new(output: &InfParOutputSys) -> InfParOutputSys {
-        output.clone()
+pub fn extend_output_state(
+    state_start: usize,
+    extra_bits: usize,
+    output: &InfParOutputSys,
+) -> UDynVarSys {
+    assert!(state_start <= output.state.bitnum());
+    if state_start + extra_bits > output.state.bitnum() {
+        output.state.clone().concat(UDynVarSys::var(
+            state_start + extra_bits - output.state.bitnum(),
+        ))
+    } else {
+        output.state.clone()
     }
 }
 
-// InitProcIdEndPosState - initialize proc id end position from memory.
+// init_mem_address_end_pos - initialize memory address end position from memory.
+// Information about MemAddressEndPos in memory:
+// At memory address 0: sequences of values between 1..=MAX and one zero,
+// MemAddressPosEndPos is sum of non-zero cells.
+// If cell_len=1 then: sequences of 1 and one zero. MemAddressPosEndPos is number of 1's.
+
+pub fn init_mem_address_end_pos_stage(
+    state_start: usize,
+    output: &InfParOutputSys,
+) -> (UDynVarSys, InfParOutputSys) {
+    (extend_output_state(state_start, 4, output), output.clone())
+}
+
+// init_proc_id_end_pos - initialize proc id end position from memory.
 // Information about ProcIdEndPos in memory: This same as in MemAddressEndPos and start
 // after MemAddressEndPos in memory.
 
-#[derive(Clone)]
-pub struct InitProcIdEndPosStage {
-    stage: UDynVarSys,   // stage indicator
-    int_stage: U3VarSys, // internal stage indicator - only for this stage.
-    state: U2VarSys,     // state for stage
-}
-
-impl InitProcIdEndPosStage {
-    pub fn new(output: &InfParOutputSys) -> InfParOutputSys {
-        output.clone()
-    }
+pub fn init_proc_id_end_pos_stage(
+    state_start: usize,
+    output: &InfParOutputSys,
+) -> (UDynVarSys, InfParOutputSys) {
+    (extend_output_state(state_start, 4, output), output.clone())
 }
