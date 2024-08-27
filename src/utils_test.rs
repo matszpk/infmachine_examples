1597590fc1b7858d98824c680c3a70388fc3e205
matszpk <matszpk@interia.pl>
2024-08-27 14:06:15 +0200

    Add init_machine_end_pos.

--- src/utils_test.rs
+++ src/utils_test.rs
@@ -207,6 +207,59 @@ fn gen_init_mem_address_end_pos_test(
     mobj.to_machine().to_toml()
 }
 
+fn gen_init_machine_end_pos_test(
+    cell_len_bits: u32,
+    data_part_len: u32,
+    temp_buffer_len: u32,
+    proc_num: u64,
+    mem_size: u64,
+    temp_buffer_step: u32,
+) -> Result<String, toml::ser::Error> {
+    let config = InfParInterfaceConfig {
+        cell_len_bits,
+        data_part_len,
+    };
+    let mut mobj = InfParMachineObjectSys::new(
+        config,
+        InfParEnvConfig {
+            proc_num,
+            flat_memory: true,
+            max_mem_size: Some(mem_size),
+            max_temp_buffer_len: temp_buffer_len,
+        },
+    );
+    mobj.in_state = Some(UDynVarSys::var(3));
+    let mut mach_input = mobj.input();
+    let unused_bit = UDynVarSys::filled(1, unused_inputs(&mobj, mach_input.state.bit(0)));
+    // first stage
+    let (output_1, _) = init_mem_address_end_pos_stage(
+        unused_bit.clone().concat(UDynVarSys::from_n(0u8, 2)),
+        unused_bit.clone().concat(UDynVarSys::from_n(1u8, 2)),
+        &mut mach_input,
+        temp_buffer_step,
+    );
+    // second stage
+    let (output_2, _) = init_proc_id_end_pos_stage(
+        unused_bit.clone().concat(UDynVarSys::from_n(1u8, 2)),
+        unused_bit.clone().concat(UDynVarSys::from_n(2u8, 2)),
+        &mut mach_input,
+        temp_buffer_step,
+    );
+    // stop stage
+    let mut output_3 = InfParOutputSys::new(config);
+    output_3.state = mach_input.state.clone();
+    output_3.stop = true.into();
+    let mut output_stages = vec![output_1, output_2, output_3.clone(), output_3];
+    InfParOutputSys::fix_state_len(&mut output_stages);
+    let final_state = dynint_table(
+        mach_input.state.clone().subvalue(1, 2),
+        output_stages.into_iter().map(|v| v.to_dynintvar()),
+    );
+    mobj.in_state = Some(mach_input.state);
+    mobj.from_dynintvar(final_state);
+    mobj.to_machine().to_toml()
+}
+
 fn main() {
     let mut args = env::args();
     args.next().unwrap();
@@ -275,6 +328,22 @@ fn main() {
             print!(
                 "{}",
                 callsys(|| gen_init_mem_address_end_pos_test(
+                    cell_len_bits,
+                    data_part_len,
+                    temp_buffer_len,
+                    proc_num,
+                    mem_size,
+                    temp_buffer_step,
+                )
+                .unwrap())
+            );
+        }
+        "init_machine_end_pos" => {
+            let temp_buffer_step: u32 = args.next().unwrap().parse().unwrap();
+            assert_ne!(temp_buffer_step, 0);
+            print!(
+                "{}",
+                callsys(|| gen_init_machine_end_pos_test(
                     cell_len_bits,
                     data_part_len,
                     temp_buffer_len,
