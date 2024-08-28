477071607cf3a85d700ea8510950dcbf2c92f8e4
matszpk <matszpk@interia.pl>
2024-08-28 13:15:16 +0200

    Add gen_move_data_pos_expr_test.

--- src/utils_test.rs
+++ src/utils_test.rs
@@ -66,6 +66,54 @@ fn gen_move_data_pos_test(
     mobj.to_machine().to_toml()
 }
 
+fn gen_move_data_pos_expr_test(
+    cell_len_bits: u32,
+    data_part_len: u32,
+    temp_buffer_len: u32,
+    proc_num: u64,
+    mem_size: u64,
+    step_num: u64,
+) -> Result<String, toml::ser::Error> {
+    let config = InfParInterfaceConfig {
+        cell_len_bits,
+        data_part_len,
+    };
+    let mut mobj = InfParMachineObjectSys::new(
+        config,
+        InfParEnvConfig {
+            proc_num,
+            flat_memory: true,
+            max_mem_size: Some(mem_size),
+            max_temp_buffer_len: temp_buffer_len,
+        },
+    );
+    mobj.in_state = Some(UDynVarSys::var(2));
+    let mut mach_input = mobj.input();
+    let unused_bit = UDynVarSys::filled(1, unused_inputs(&mobj, mach_input.state.bit(1)));
+    // first stage
+    let (output_1, _) = move_data_pos_expr_stage(
+        UDynVarSys::from_n(0u8, 1).concat(unused_bit.clone()),
+        UDynVarSys::from_n(1u8, 1).concat(unused_bit.clone()),
+        &mut mach_input,
+        DKIND_TEMP_BUFFER,
+        DPMOVE_FORWARD,
+        UDynVarSys::from_n(step_num.checked_sub(1).unwrap(), 64),
+    );
+    // stop stage
+    let mut output_2 = InfParOutputSys::new(config);
+    output_2.state = UDynVarSys::from_n(1u8, 1).concat(unused_bit.clone());
+    output_2.stop = true.into();
+    let mut output_stages = vec![output_1, output_2];
+    InfParOutputSys::fix_state_len(&mut output_stages);
+    let final_state = dynint_table(
+        mach_input.state.clone().subvalue(0, 1),
+        output_stages.into_iter().map(|v| v.to_dynintvar()),
+    );
+    mobj.in_state = Some(mach_input.state);
+    mobj.from_dynintvar(final_state);
+    mobj.to_machine().to_toml()
+}
+
 fn gen_move_data_pos_and_back_test(
     cell_len_bits: u32,
     data_part_len: u32,
@@ -226,6 +274,22 @@ fn main() {
             print!(
                 "{}",
                 callsys(|| gen_move_data_pos_test(
+                    cell_len_bits,
+                    data_part_len,
+                    temp_buffer_len,
+                    proc_num,
+                    mem_size,
+                    step_num,
+                )
+                .unwrap())
+            );
+        }
+        "move_data_pos_expr" => {
+            let step_num: u64 = args.next().unwrap().parse().unwrap();
+            assert_ne!(step_num, 0);
+            print!(
+                "{}",
+                callsys(|| gen_move_data_pos_expr_test(
                     cell_len_bits,
                     data_part_len,
                     temp_buffer_len,
