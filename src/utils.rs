bcb2039d99e68dc834fdf207638ad73023119452
matszpk <matszpk@interia.pl>
2024-08-30 13:04:58 +0200

    Small fix in Add1Func.

--- src/utils.rs
+++ src/utils.rs
@@ -537,7 +537,11 @@ impl Add1Func {
     pub fn new_from_u64(inout_len: usize, value: u64) -> Self {
         Self {
             inout_len,
-            value: UDynVarSys::from_n(value, calc_log_bits_u64(value)),
+            value: if value != 0 {
+                UDynVarSys::from_n(value, calc_log_bits_u64(value))
+            } else {
+                UDynVarSys::from_n(value, 1)
+            },
         }
     }
 }
